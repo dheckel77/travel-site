9b3c90c65e4f0ef576a97e7c7cbddd0fcfce3b7e
dheckel77 <dheckel77@gmail.com>
2019-05-23 16:34:22 -0400

    Added Sticky Header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/Sticky-Header';
 
 let mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+let stickyHeader = new StickyHeader();
